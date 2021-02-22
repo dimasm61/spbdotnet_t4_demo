5dca0ce13507d76468e372f0630890bcda98fb2f
Dimasm <dimasm_mail@mail.ru>
2021-02-22 11:07:42 +0300

    простой кодогенератор

--- MyCodegenerator/Program.cs
+++ MyCodegenerator/Program.cs
@@ -6,7 +6,30 @@ namespace MyCodegenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nameSpace = (args.Length > 0) ? args[0] : "defaultNamespace";
+            var enumName = (args.Length > 1) ? args[1] : "defaultEnumName";
+            var enumValues = (args.Length > 2) ? args[2] : "defValue1:;defValue2:";
+
+            var enumValues2 = enumValues.Split(';');
+
+            Console.WriteLine($"namespace {nameSpace}");
+            Console.WriteLine($"{{");
+            {
+                Console.WriteLine($"    public enum {enumName}");
+                Console.WriteLine($"    {{");
+                {
+                    foreach (var e in enumValues2)
+                    {
+                        var a = e.Split(':');
+                        Console.WriteLine($"");
+                        Console.WriteLine($"        /// <summary>{a[1]}</summary>");
+                        Console.WriteLine($"        {a[0]},");
+                    }
+
+                }
+                Console.WriteLine($"    }}");
+            }
+            Console.WriteLine($"}}");
         }
     }
 }
---
+++ MyCodegeneratorUsing/MyCodegenRun.cmd
@@ -0,0 +1,6 @@
+﻿cd /d %~dp0
+..\MyCodegenerator\bin\Debug\net5.0\MyCodegenerator.exe ^
+ MyCodegeneratorUsing ^
+ DemoCountryEnum ^
+ AUS:Australia;AUT:Austria;BHR:Bahrain;BLR:Belarus;BRA:Brazil;FIN:Finland;FRA:France;DEU:Germany;POL:Poland;RUS:Russian Federation;^
+ > MyCodegen.cs
